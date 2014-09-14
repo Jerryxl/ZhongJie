5a623f48782981b8af44c155e800958cff738db7
hitxueliang <hitxueliang@163.com>
2014-09-14 16:01:05 +0800

    ok

---
+++ ZhongJieNeng/src/com/zj/vo/ImageVO.java
@@ -0,0 +1,19 @@
+package com.zj.vo;
+
+public class ImageVO {
+
+	private String imagename;
+	private String description;
+	public String getImagename() {
+		return imagename;
+	}
+	public void setImagename(String imagename) {
+		this.imagename = imagename;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
